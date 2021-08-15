f972997b149534c9ef087f5103491e27b47737f3
Gonda92 <egonda1992@gmail.com>
2021-08-15 17:52:05 -0500

    footer modificado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar";
-
+import Footer from "./Footer";
 
 
 function App() {
   return (
     <div className="container">
       <Navbar />
+      <Footer/>
     </div>
   );
 }
