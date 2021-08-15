5d3481e498e2ce5734cd9644eb207fd7a99c35e8
Baixo13 <nicolasferrerb@gmail.com>
2021-08-15 18:48:57 -0400

    add Form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Form from "./Form";
 import Navbar from "./Navbar";
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="container">
       <Navbar />
+      <Form/>
     </div>
   );
 }
