34b32e3424344f4b4d10e252d08a8830a211462d
Francisco <franciscozamoranovera@gmail.com>
2021-08-29 16:42:10 -0400

    Table created

---
+++ src/Table.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Table = () => {
+    return (
+        <>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="table-success">
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr className="table-info">
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr className="table-danger">
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr className="table-warning">
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Horse</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Table
